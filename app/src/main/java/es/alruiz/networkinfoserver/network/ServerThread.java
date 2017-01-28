fe8f9d5c950d2ac9ac49c533cfeb13e7f8d46ba0
Alfonso <alruiz85@gmail.com>
2017-01-28 20:33:24 +0100

    added server thread

---
+++ app/src/main/java/es/alruiz/networkinfoserver/network/ServerThread.java
@@ -0,0 +1,13 @@
+package es.alruiz.networkinfoserver.network;
+
+/**
+ * Created by Alfonso on 28/01/2017.
+ */
+
+public class ServerThread implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
